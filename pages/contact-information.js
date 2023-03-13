85ba7dba737e177b4165247ba26a838979fd98c2
Sanjay Suthar <sanjay.suthar@fablefintech.com>
2023-03-13 23:33:45 +0530

    1st phase done

--- pages/contact-information.js
+++ pages/contact-information.js
@@ -4,7 +4,9 @@ export default function ContactInformation() {
   return (
     <>
       <Main seotitle="Contact Information" title={<>Contact Information</>}>
-        <div className="container"><p>To be updated soon</p></div>
+        <div className="container">
+          <p>To be updated soon</p>
+        </div>
       </Main>
     </>
   );
--- pages/floor-map.js
+++ pages/floor-map.js
@@ -4,7 +4,9 @@ export default function FloorMap() {
   return (
     <>
       <Main seotitle="Floor Map" title={<>Floor Map</>}>
-        <div className="container"><p>To be updated soon</p></div>
+        <div className="container">
+          <p>To be updated soon</p>
+        </div>
       </Main>
     </>
   );
